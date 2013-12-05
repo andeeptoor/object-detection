259fef6d45d35ae4ca52bd864fbd11f36e0f2820
andeep.toor <andeep.toor@gmail.com>
2013-12-05 11:18:30 -0500

    Added custom trained hog face model

--- scripts/createPositiveSamples.sh
+++ scripts/createPositiveSamples.sh
@@ -3,7 +3,7 @@
 inputFile=$1
 outputFile=$2
 numberOfSamples=$3
-width=20
-height=20
+width=19
+height=19
 
 opencv_createsamples -info $inputFile -vec $outputFile -w $width -h $height -num $numberOfSamples
--- scripts/trainCascade.sh
+++ scripts/trainCascade.sh
@@ -3,10 +3,10 @@
 positiveExamples=$1
 negativeExamples=$2
 featureType=$3
-negativeNumber=2247
-positiveNumber=8877
+negativeNumber=28121
+positiveNumber=2601
 bufferSize=4048
-width=20
-height=20
+width=19
+height=19
 
 opencv_traincascade -data . -vec $positiveExamples -bg $negativeExamples -numPos $positiveNumber -numNeg $negativeNumber -numStages 20 -precalcValBufSize $bufferSize -precalcIdxBufSize $bufferSize -stageType BOOST -featureType $featureType -h $height -w $width -minHitRate 0.999 -maxFalseAlarmRate 0.5
